39e795516c85dd3172a0d3b1a92f4ec36bbf7d9e
Mohamed Elhar <elhar.mohamed@gmail.com>
2020-03-03 15:48:28 +0100

    A customer have a balance of $

--- src/main/java/com/indev/cryptocurrency/exchange/Customer.java
+++ src/main/java/com/indev/cryptocurrency/exchange/Customer.java
@@ -1,26 +1,38 @@
 package com.indev.cryptocurrency.exchange;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 public class Customer {
+	
+	private final Log LOGGER = LogFactory.getLog(Customer.class);
     private String bitcoin;
     private int i;
+    private int balance;
 
     public Customer(){}
 
-    public Customer(String bitcoin, int i){
+    public Customer(String bitcoin, int i, int balance){
         this.bitcoin = bitcoin;
         this.i = i;
+        this.balance = balance;
     }
 
+    public Customer(int balance){
+        this.balance = balance;
+    }
+    
     public Customer withCryptocurrency(String bitcoin, int i) {
-        return new Customer(bitcoin, i);
+        return new Customer(bitcoin, i, this.balance);
     }
 
     public Customer withBalance(int i) {
-        return null;
+        return new Customer(i);
     }
 
     @Override
     public String toString() {
-        return "0:$,"+i+":"+bitcoin;
+    	LOGGER.info("******** "+balance+":$,"+i+":"+bitcoin+"***********");
+        return balance+":$,"+i+":"+bitcoin;
     }
 }
