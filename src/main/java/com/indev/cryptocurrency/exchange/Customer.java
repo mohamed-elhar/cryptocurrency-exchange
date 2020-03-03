f5eaf7468a826d0143f9132ba3a6a43a5dda5c56
Mohamed Elhar <elhar.mohamed@gmail.com>
2020-03-03 15:33:39 +0100

    A customer may have a given quantity of a cryptocurrency in his wallet

--- src/main/java/com/indev/cryptocurrency/exchange/Customer.java
+++ src/main/java/com/indev/cryptocurrency/exchange/Customer.java
@@ -1,11 +1,26 @@
 package com.indev.cryptocurrency.exchange;
 
 public class Customer {
+    private String bitcoin;
+    private int i;
+
+    public Customer(){}
+
+    public Customer(String bitcoin, int i){
+        this.bitcoin = bitcoin;
+        this.i = i;
+    }
+
     public Customer withCryptocurrency(String bitcoin, int i) {
-        return null;
+        return new Customer(bitcoin, i);
     }
 
     public Customer withBalance(int i) {
         return null;
+    }
+
+    @Override
+    public String toString() {
+        return "0:$,"+i+":"+bitcoin;
     }
 }
