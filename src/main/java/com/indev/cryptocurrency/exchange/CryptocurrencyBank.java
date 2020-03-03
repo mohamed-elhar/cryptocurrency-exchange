ff3584dea3cc35118ba46f18bfcb19733b4d794c
Mohamed Elhar <elhar.mohamed@gmail.com>
2020-03-03 17:08:41 +0100

    shouldBuyCryptocurrencyMetcalfeLaw

--- src/main/java/com/indev/cryptocurrency/exchange/CryptocurrencyBank.java
+++ src/main/java/com/indev/cryptocurrency/exchange/CryptocurrencyBank.java
@@ -4,32 +4,42 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CryptocurrencyBank {
-	
+
 	private Customer sellerCustomer;
 	private List<String> supportedCryptoCurrency = new ArrayList<>();
-	
-    public void addSupportedCryptoCurrency(String bitcoin) {
-        this.supportedCryptoCurrency.add(bitcoin);
-    }
+	private int buyerCount = 0;
 
-    public int requestTransaction(Customer buyerCustomer, int i, String bitcoin) {
-    	int ret = 0;
-    	if(sellerCustomer == null)
-    		return ret;
-    	else {
-    		if(sellerCustomer.getBitcoin() == bitcoin) {
-    			sellerCustomer.setBalance(i);
-    			sellerCustomer.setI(sellerCustomer.getI() - i);
-    			buyerCustomer.setBalance(buyerCustomer.getBalance() - i);
-    			buyerCustomer.setI(i);
-    			buyerCustomer.setBitcoin(bitcoin);
-    			ret = i;
-    		}
-    		return ret;
-    	}
-    }
+	public void addSupportedCryptoCurrency(String bitcoin) {
+		this.supportedCryptoCurrency.add(bitcoin);
+	}
 
-    public void addSeller(Customer sellerCustomer) {
-    	this.sellerCustomer = sellerCustomer;
-    }
+	public int requestTransaction(Customer buyerCustomer, int i, String bitcoin) {
+		int ret = 0;
+		if (sellerCustomer == null)
+			return ret;
+		else {
+			if (sellerCustomer.getBitcoin() == bitcoin) {
+				buyerCount += 1;
+				if (buyerCount == 1) {
+					sellerCustomer.setBalance(sellerCustomer.getBalance() + i);
+					buyerCustomer.setBalance(
+							buyerCustomer.getBalance() - i);
+				} else {
+					sellerCustomer.setBalance(
+							sellerCustomer.getBalance() + (i * ((buyerCount * buyerCount - 1) - (buyerCount - 1))));
+					buyerCustomer.setBalance(
+							buyerCustomer.getBalance() - (i * ((buyerCount * buyerCount - 1) - (buyerCount - 1))));
+				}
+				sellerCustomer.setI(sellerCustomer.getI() - i);
+				buyerCustomer.setI(i);
+				buyerCustomer.setBitcoin(bitcoin);
+				ret = i;
+			}
+			return ret;
+		}
+	}
+
+	public void addSeller(Customer sellerCustomer) {
+		this.sellerCustomer = sellerCustomer;
+	}
 }
